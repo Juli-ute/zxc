98c6c7d33cc3264726a27ef2eb1babf2c17995dc
Juli-ute <161311679+Juli-ute@users.noreply.github.com>
2024-03-25 14:36:41 +0300

    Add files via upload

--- itc_len_num.cpp
+++ itc_len_num.cpp
@@ -4,7 +4,10 @@
 
 int itc_len_num(long long ch){
     int result = 0;
-    while(ch !=0){
+    if (ch == 0){
+        return 1;
+    }
+    while(ch != 0){
             ch = ch / 10;
         result += 1;
     }
