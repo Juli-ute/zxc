96842719c799ed194970878223b333441d15570f
Juli-ute <161311679+Juli-ute@users.noreply.github.com>
2024-03-25 15:16:20 +0300

    Add files via upload

--- itc_sum_num.cpp
+++ itc_sum_num.cpp
@@ -2,9 +2,12 @@
 #include "middle.h"
 
 int itc_sum_num(long long ch){
+    if (ch < 0){
+        ch = ch * -1;
+    }
     int a = 0;
     int result = 0;
-    while(ch !=0){
+    while(ch != 0){
             a = ch % 10;
             ch = ch / 10;
         result += a;
