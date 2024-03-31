45054d088cda8d4ad1ff43dd54c3f0e7e588b52a
Juli-ute <161311679+Juli-ute@users.noreply.github.com>
2024-03-31 20:36:56 +0300

    Update itc_multi_num.cpp

--- itc_multi_num.cpp
+++ itc_multi_num.cpp
@@ -3,12 +3,17 @@
 
 long long itc_multi_num(long long ch){
     long long a = 0;
+    long long b = 0;
     long long result = 1;
+    if(ch < 0){
+        b = 1;}
+
     while(ch !=0){
             a = ch % 10;
             ch = ch / 10;
         result *= a;
-//cout << "a=" << a << " h=" << ch << " result=" << result;
         }
+        if(b == 1){
+        return -result;}
   return result;
 }
