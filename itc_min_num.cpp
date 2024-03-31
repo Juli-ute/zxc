760d36a4ebb61da0938be9a7e52e1cc0dd0d1626
Juli-ute <161311679+Juli-ute@users.noreply.github.com>
2024-03-31 21:01:05 +0300

    Update itc_min_num.cpp

--- itc_min_num.cpp
+++ itc_min_num.cpp
@@ -4,6 +4,9 @@
 int itc_min_num(long long ch){
     long long a = 0;
     long long b = 10;
+     if (ch < 0){
+        ch = ch * -1;
+    }
     while(ch !=0){
             a = ch % 10;
             if (a < b){
