a73e30c1d283095c90bd848547bd151f5b42e402
Juli-ute <161311679+Juli-ute@users.noreply.github.com>
2024-04-12 01:39:54 +0300

    Update itc_second_max_num.cpp

--- itc_second_max_num.cpp
+++ itc_second_max_num.cpp
@@ -2,29 +2,29 @@
 #include "middle.h"
 
 int itc_second_max_num(long long ch){
+    if (ch < 0){
+        ch = ch * -1;
+    }
     if (ch < 10){
         return -1;
     }
+    long long ch2 = ch;
     long long a = 0;
     long long a2 = 0;
     long long b = 0;
-    long long b2 = 0;
     while(ch !=0){
             a = ch % 10;
             if (a > b){
             b = a;
             }
+             ch = ch / 10;
     }
-     while(ch !=0){
-            a = ch % 10;
-
-            if (a > a2){
+    while(ch !=0){
+            a = ch2 % 10;
+            if (a > a2 and a < b){
             a2 = a;
             }
-            if (a2 < b){
-            b2 = a2;
-            }
-            ch = ch / 10;
+            ch2 = ch2 / 10;
         }
-  return b2;
+  return a2;
 }
