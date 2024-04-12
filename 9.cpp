d49cf355c6b429c96a2baa6bd8ca57ac34f4eae8
Juli-ute <161311679+Juli-ute@users.noreply.github.com>
2024-04-12 23:15:36 +0300

    Add files via upload

---
+++ 9.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+bool itc_mirror_num(long long ch){
+
+int a = 0;
+long long b = 0;
+long long ch2 = ch;
+
+while (ch != 0){
+    a = ch % 10;
+    b = b * 10 + a;
+    ch = ch / 10;
+}
+
+if(b == ch2){
+    return 1;
+}
+else{
+    return 0;
+}
+}
