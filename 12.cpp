ae7dbd07ff2b264947edf85beef26316bb9acf0f
Juli-ute <161311679+Juli-ute@users.noreply.github.com>
2024-04-12 23:06:01 +0300

    Add files via upload

---
+++ 12.cpp
@@ -0,0 +1,36 @@
+
+#include <iostream>
+#include "middle.h"
+
+int itc_second_simple_max_num(long long ch){
+    if (ch < 0){
+        ch = ch * -1;
+    }
+    long long a = 0;
+    long long b2 = -1;
+    long long b = -1;
+
+     if (ch < 10){
+        return -1;
+    }
+ else{
+    while (ch != 0){
+        a = ch % 10;
+        if(b2 <= a){
+            b = b2;
+            b2 = a;
+        }
+        if(a < b2 && a > b){
+            b = a;
+        }
+        ch = ch / 10;
+    }
+    if (b2 == b){
+        return -1;
+    }
+    else{
+return b;
+    }
+}
+}
+
