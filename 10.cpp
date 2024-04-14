376e5058a4e0b194020858a4a266d07ad3de460f
Juli-ute <161311679+Juli-ute@users.noreply.github.com>
2024-04-14 16:24:27 +0300

    Add files via upload

---
+++ 10.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include "middle.h"
+using namespace std;
+
+int itc_mirror_count(long long ch){
+
+int risult = 0;
+long long a = 1;
+
+while (a != ch){
+    if(itc_mirror_num(a)){
+        risult++;}
+    a++;
+    }
+return risult;
+}
