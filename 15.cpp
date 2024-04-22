a69779a1040687ac744e7131ea9f2854c1264651
Juli-ute <161311679+Juli-ute@users.noreply.github.com>
2024-04-22 13:06:43 +0300

    Update 15.cpp

--- 15.cpp
+++ 15.cpp
@@ -2,6 +2,7 @@
 #include "middle.h"
 using namespace std;
 
+
 long long itc_rev_bin_num(long long ch){
 
 long long b = 0;
@@ -9,22 +10,24 @@ long long a = 0;
 long long c = 0;
 long long c2 = 0;
 long long j = 1;
-long long jj = 0;
+long long j2 = 1;
 
 while(ch != 0){
     a = ch % 10;
     if(a == 1){
     c2 = c;
-    while(c2 <= 0){
-      j *= j;
+    while(c2 > 1){
+      j2 = j2 * j;
       c2 -= 1;
     }
-     b += j;
+     b += j2;
     }
     j = 2;
+    j2 = 2;
     c += 1;
     ch = ch / 10;
     }
 
 return b;
 }
+
